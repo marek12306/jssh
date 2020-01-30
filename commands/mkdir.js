0c731941566da232e82497fb331d771150d18631
Dawid <39349444+Indexerrowaty@users.noreply.github.com>
2020-01-30 01:18:39 +0100

    Added mkdir command

---
+++ commands/mkdir.js
@@ -0,0 +1,19 @@
+const {resolve} = require("path");
+const fs = require("fs");
+
+module.exports.execute = (cwd, args, commands, cd) => {
+    return new Promise((reso,rej) => {
+        cwd = resolve(cwd, args.join(" "));
+
+if (!fs.existsSync(cwd)){
+    fs.mkdirSync(cwd);
+    return reso();
+} else {
+    return rej('Folder ' + cwd + ' exists.');
+}
+    });
+}
+
+module.exports.description = 
+`Creates folder
+mkdir [folder name]`;
